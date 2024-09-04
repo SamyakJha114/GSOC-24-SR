576208968a1e4aeb4298c832fb3cb90ca0c4da3d
SamyakJha114 <samyakjha71@gmail.com>
2024-09-04 10:12:38 -0700

    changes

--- GP+Beam_search/runs/run_job_only_gp.sh
+++ GP+Beam_search/runs/run_job_only_gp.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-task=2
 #SBATCH --cpus-per-task=64          
-#SBATCH --time=05:00:00                   
+#SBATCH --time=08:00:00                   
 #SBATCH --output=/global/homes/s/samyak09/GSOC-SR/GSOC-24-SR/GP+Beam_search/runs/runs_only_gp%j.out            
 #SBATCH --error=/global/homes/s/samyak09/GSOC-SR/GSOC-24-SR/GP+Beam_search/runs/runs_only_gp%j.err             
 #SBATCH --mail-type=END,FAIL              
