f739cf2bce74c58d5290b55ee5282a77b4fe65fd
SamyakJha114 <samyakjha71@gmail.com>
2024-11-21 07:59:40 -0800

    ch

--- DPOFineTuning/gp.py
+++ DPOFineTuning/gp.py
@@ -229,7 +229,7 @@ def run_gp(toolbox, points, seed_expr, pset, num_cores=None):
 
     return pop, log, hof
 
-def generate_preference_pairs(population, points,pset,top_n=2,middle_n =3,compare_with_n=100):
+def generate_preference_pairs(population, points,pset,top_n=20,middle_n =0,compare_with_n=20):
     print("GENERATING PREFERENCE PAIRS")
     top_individuals = tools.selBest(population, top_n)
     compare_individuals = tools.selBest(population[middle_n:], compare_with_n)
